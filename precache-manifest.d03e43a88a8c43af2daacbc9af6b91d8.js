efeb987a5004dee0486dc4601aa6da23baa352b5
Glory Adedayo <gloryadedayo10@gmail.com>
2020-07-30 00:12:46 -0500

    push to github

---
+++ precache-manifest.d03e43a88a8c43af2daacbc9af6b91d8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff3d957e7a9bb2e1c1b63c88577da8a9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ff3fe88620f5c8e06141",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "9015b5ed546e87ba2ce2",
+    "url": "/static/css/main.fd3f91ff.chunk.css"
+  },
+  {
+    "revision": "ff3fe88620f5c8e06141",
+    "url": "/static/js/2.f31aacbf.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.f31aacbf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9015b5ed546e87ba2ce2",
+    "url": "/static/js/main.86b39e33.chunk.js"
+  },
+  {
+    "revision": "6c28f437ef69993a1d7f",
+    "url": "/static/js/runtime-main.07542463.js"
+  },
+  {
+    "revision": "3d138e255396ebc8bdbdbf79cfee8107",
+    "url": "/static/media/new_logo.3d138e25.jpeg"
+  }
+]);
